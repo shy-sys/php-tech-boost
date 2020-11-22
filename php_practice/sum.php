456bf2751287ca04b675cef8162a59ae0f004a65
”nakai” <y.b.m.s.8132@gmail.com>
2020-11-22 14:35:17 +0000

    first commit

--- php_practice/sum.php
+++ php_practice/sum.php
@@ -1,21 +1,44 @@
+//課題１
 <?php
-//1~10 までを足した値を返す関数
-function sum(){
-	
-	//$result は結果を保存する変数
-	$result = 0;
-	
-	//$i　は　1　から始まり　10 より大きくなるまでループする
-	for($i = 1; $i <= 10; $i++){
-		
-	//$result に　$i を順番に足していく
-	$result += $i;
-}
-
-//$result を結果として返す
-return $result;
-}
-
-//関数を実行する
-echo sum();
+function sum($max){
+	$result =$max*2;
+	return $result;
+}	
+	echo sum(100);
 ?>
+
+//課題２
+<?php
+function f($a, $b){
+	$sum = $a +$b;
+	return $sum;
+}
+$result=f(2,2);
+echo $result;
+echo "/n";
+?>
+
+//課題３
+<?php
+$arr =array(1,3,5,7,9);
+foreach($arr as $a){
+	echo "/n";
+}
+
+//応用
+<?php
+ function max_array($arr){
+// とりあえず配列の最初の要素を一番大きい値とする
+ $max_number = $arr[0];
+ foreach($arr as $a){
+ 	if ($max_number < $a){
+ 		$max_number =$a;
+ 	}
+ //どうしたらいいかわからない・・・・
+ }
+ return $max_number;
+ }
+ echo max_array (1,10,20,30);
+ echo "/n";
+ ?>
+ 
