4002dd2d110e01d20f60973c88a291fea5154b08
”nakai” <y.b.m.s.8132@gmail.com>
2020-12-09 06:00:24 +0000

    add file

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -21,6 +21,13 @@ Route::group(['prefix' => 'admin'], function() {
 
 Route::group(['prefix' => 'admin'], function() {
 Route::get('news/create', 'Admin\NewsController@add');
-Route::get('news/delete', 'Admin\ProfileController@delete');
-Route::get('news/update', 'Admin\ProfileController@update');
+Route::get('news/delete', 'Admin\NewsController@delete');
+Route::get('news/update', 'Admin\NewsController@update');
+});
+
+
+Route::group(['prefix' => 'admin'], function() {
+Route::get('news/create', 'Admin\ProfileController@add');
+Route::get('news/edit', 'Admin\ProfileController@edit');
+
 });
