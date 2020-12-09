8f42e7b67bd34186f8f4a6f7eb7b9d4b946c560c
”nakai” <y.b.m.s.8132@gmail.com>
2020-12-09 13:00:13 +0000

    add file

--- mynews/resources/views/admin/profile/create.blade.php
+++ mynews/resources/views/admin/profile/create.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>MyProfile</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        <h1>My　プロフィール作成画面</h1>
     </body>
 </html>
