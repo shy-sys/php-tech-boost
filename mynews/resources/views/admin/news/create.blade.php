f4c40499ecf507aa710492e5795f83877c41334d
”nakai” <y.b.m.s.8132@gmail.com>
2021-01-07 08:49:28 +0000

    add file

--- mynews/resources/views/admin/news/create.blade.php
+++ mynews/resources/views/admin/news/create.blade.php
@@ -16,27 +16,27 @@
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
+                            <li>{{ $e }}</li>
                             @endforeach
                         </ul>
                     @endif
                     <div class="form-group row">
                         <label class="col-md-2">タイトル</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
-                    <div class="form-group row">
+                        <div class="form-group row">
                         <label class="col-md-2">本文</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
                     </div>
-                    <div class="form-group row">
+                        <div class="form-group row">
                         <label class="col-md-2">画像</label>
                         <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
+                        <input type="file" class="form-control-file" name="image">
+                       </div>
                     </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -15,16 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-  Route::get('news/create', 'Admin\NewsController@add');
-  Route::post('news/create', 'Admin\NewsController@create');
-  Route::get('news/edit', 'Admin\NewsController@edit');
-  Route::post('news/edit', 'Admin\NewsController@update');
-  Route::get('news/delete', 'Admin\NewsController@delete');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+  Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+  Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+  Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+  Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+  Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
 
-  Route::get('profile/create', 'Admin\ProfileController@add');
-  Route::post('profile/create', 'Admin\ProfileController@create');
-  Route::get('profile/edit', 'Admin\ProfileController@edit');
-  Route::post('profile/edit', 'Admin\ProfileController@update');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+  Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+  Route::get('profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
 });
 
 Auth::routes();
